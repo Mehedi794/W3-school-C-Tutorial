a6db9c96c26931f993885c7d29e24efef64cadd2
Mehedi Rana <91437905+Mehedi794@users.noreply.github.com>
2024-01-13 01:34:22 +0000

    Add files via upload

---
+++ app_w3.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Mehedi Rana on 13/01/2024.
+//
+
+#include "app_w3.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+
+}
---
+++ app_w3.h
@@ -0,0 +1,14 @@
+//
+// Created by Mehedi Rana on 13/01/2024.
+//
+
+#ifndef W3_SCHOOL_C_TUTORIAL_APP_W3_H
+#define W3_SCHOOL_C_TUTORIAL_APP_W3_H
+
+
+class app_w3 {
+
+};
+
+
+#endif //W3_SCHOOL_C_TUTORIAL_APP_W3_H
